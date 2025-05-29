56cf4c2d52a5dc6bbba7b82ba7f06bfa67580ac1
unknown <aaa@Aaa.com>
2025-05-29 20:52:29 -0300

    first commit

---
+++ src/main/java/org/example/KMeans.java
@@ -0,0 +1,89 @@
+package org.example;
+
+import java.util.*;
+
+public class KMeans {
+    private int k;
+    private double[][] centroids;
+    private List<List<double[]>> clusters;
+
+    public KMeans(int k) {
+        this.k = k;
+    }
+
+    private void initializeCentroids(double[][] data) {
+        Random rand = new Random();
+        centroids = new double[k][data[0].length];
+        for (int i = 0; i < k; i++) {
+            int randomIndex = rand.nextInt(data.length);
+            centroids[i] = data[randomIndex];
+        }
+    }
+
+    private void assignClusters(double[][] data) {
+        clusters = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            clusters.add(new ArrayList<>());
+        }
+        for (double[] point : data) {
+            int nearestCentroid = getNearestCentroid(point);
+            clusters.get(nearestCentroid).add(point);
+        }
+    }
+
+    private int getNearestCentroid(double[] point) {
+        int nearestIndex = 0;
+        double minDistance = Double.MAX_VALUE;
+        for (int i = 0; i < k; i++) {
+            double distance = euclideanDistance(point, centroids[i]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestIndex = i;
+            }
+        }
+        return nearestIndex;
+    }
+
+    private double euclideanDistance(double[] point1, double[] point2) {
+        double sum = 0.0;
+        for (int i = 0; i < point1.length; i++) {
+            sum += Math.pow(point1[i] - point2[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    private void updateCentroids() {
+        for (int i = 0; i < k; i++) {
+            double[] newCentroid = new double[centroids[i].length];
+            for (double[] point : clusters.get(i)) {
+                for (int j = 0; j < point.length; j++) {
+                    newCentroid[j] += point[j];
+                }
+            }
+            for (int j = 0; j < newCentroid.length; j++) {
+                newCentroid[j] /= clusters.get(i).size();
+            }
+            centroids[i] = newCentroid;
+        }
+    }
+
+    public static void main(String[] args) {
+        double[][] data = {
+                {1.0, 2.0},
+                {1.5, 1.8},
+                {5.0, 8.0},
+                {8.0, 8.0},
+                {1.0, 0.6},
+                {9.0, 11.0}
+        };
+        KMeans kMeans = new KMeans(2);
+        kMeans.initializeCentroids(data);
+        kMeans.assignClusters(data);
+        kMeans.updateCentroids();
+        System.out.println("-------------------------- CLUSTER ----------------------------");
+       
+    }
+
+
+
+}
