942b41c4fe335e89e64c783ad17b4f7c7977377f
Willian Kakihata <161545901+WillianKakihata@users.noreply.github.com>
2025-06-13 18:47:40 -0300

    Update Pessoa.java

--- src/main/java/org/example/Pessoa.java
+++ src/main/java/org/example/Pessoa.java
@@ -1,19 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Pessoa {
     String nome;
     int idade;
     int faltas;
+    String categoria; // Rótulo da classe
 
-    public Pessoa(String nome, int idade, int faltas) {
+    public Pessoa(String nome, int idade, int faltas, String categoria) {
         this.nome = nome;
         this.idade = idade;
         this.faltas = faltas;
-
+        this.categoria = categoria;
     }
 
     @Override
     public String toString() {
-        return nome + " | Idade: " + idade + " | Faltas: " + faltas;
+        return "Nome: " + nome +
+                " | Idade: " + idade +
+                " | Faltas: " + faltas +
+                " | Categoria: " + categoria;
+    }
+
+    public static class Cluster {
+        double centroid;
+        List<Pessoa> pessoas = new ArrayList<>();
+        Map<String, List<Pessoa>> subgrupos = new HashMap<>();
     }
 }
